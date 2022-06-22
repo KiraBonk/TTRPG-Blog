62d145b0f717b311b7e4baac68a8b9f43637bee0
KiraBonk <videostuff16@gmail.com>
2022-06-22 15:49:21 -0300

    clean up

--- pages/characters/[characterId].tsx
+++ pages/characters/[characterId].tsx
@@ -1,10 +1,4 @@
-import { useRouter } from "next/router";
-
 function UniqueCharacterPage() {
-  const router = useRouter();
-
-  const characterId = router.query.characterId;
-
   return (
     <div>
       <h1>this is a characters page</h1>
