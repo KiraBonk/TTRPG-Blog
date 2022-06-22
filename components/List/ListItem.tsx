51bcce580e36605663474e65595e7a18b35d2b97
KiraBonk <videostuff16@gmail.com>
2022-06-22 16:25:40 -0300

    detail button now links to dynamically

--- components/List/ListItem.tsx
+++ components/List/ListItem.tsx
@@ -2,19 +2,28 @@ import Card from "../Card/Card";
 import classes from "./ListItem.module.css";
 import Image from "next/image";
 
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 function ListItem(props: any) {
+  // const router = useRouter();
+
+  // function showDetailsHandler() {
+  //   router.push('/')
+  // }
+
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <Image src={props.image} alt={props.title} width='200' height='200' />
+          <Image src={props.image} alt={props.title} width="200" height="200" />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button><Link href={`/characters/${props.id}`}>Show Details</Link></button>
         </div>
       </Card>
     </li>
